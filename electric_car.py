07c160ab8ef3809312b2173d33d0b04e621959d3
Chukwukagideon <Chukwukagideon5@gmail.com>
2024-12-17 04:42:31 +0100

    ----Instance As Attributes--- Created a battery class to model the battery of an electric car, then set an instance of the battery as an attribute in the electric car

--- electric_car.py
+++ electric_car.py
@@ -34,13 +34,47 @@ class Car:
         else:
             print("You can't reduce the odometer reading")
 
+    def fill_gas_tank(self):
+        """print a statement to indicate that the gas tank has been filled"""
+        print(f"Gas tank filled successfully")
+
+
+class Battery:
+    """A simple attempt to model a battery for an electric car"""
+    def __init__(self, battery_size=75):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """print a statement describing the battery size"""
+        print(f"This car has a {self.battery_size}-KWh battery size")
+
+    def get_range(self):
+        """"Print a statement about the range this battery provides"""
+        if self.battery_size == 75:
+            car_range = 260
+        elif self.battery_size == 100:
+            car_range = 315
+
+        print(f"This car can go about {car_range} miles on a full charge")
+
 
 class ElectricCar(Car):
     """Represents aspects of a car, specific to electric vehicles."""
     def __init__(self, make, model, year):
-        """Initialize attributes of the parent class"""
+        """Initialize attributes of the parent class.
+        Then initialize attributes specific to an electric car"""
         super().__init__(make, model, year)
+        self.battery = Battery()
+
+
+    def fill_gas_tank(self):
+        """Electric cars don't have gas tank, hence print a statement to overide
+        the statement from the parent class"""
+        print("This car doesn't need a gas tank")
 
 
 my_tesla = ElectricCar("tesla", "model s", 2019)
 print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
