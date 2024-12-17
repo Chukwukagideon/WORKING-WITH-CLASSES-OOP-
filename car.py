31da2e924c9fcd59e7aa420c781cf95b34df457d
Chukwukagideon <Chukwukagideon5@gmail.com>
2024-12-17 03:12:07 +0100

    modifying an attribute value directly

--- car.py
+++ car.py
@@ -21,3 +21,8 @@ my_new_car = Car("audi", "a4", 2019)
 print(my_new_car.get_descriptive_name())
 my_new_car.read_odometer()
 
+# Modifying attribute values can be done in 3 ways
+# Modifying an attribute value directly
+my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
+
