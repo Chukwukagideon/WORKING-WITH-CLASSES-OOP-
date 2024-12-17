d25394bf3025e46513a754b375613bd425553fad
Chukwukagideon <Chukwukagideon5@gmail.com>
2024-12-17 03:51:12 +0100

    added attribute to show number of customers served. also created a method to update the number of customers served.

--- restaurant.py
+++ restaurant.py
@@ -4,6 +4,8 @@ class Restaurant:
         """initialising restaurant attributes"""
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = 0
+
 
     def describe_restaurant(self):
         print(f"Welcome to {self.restaurant_name}")
@@ -11,6 +13,10 @@ class Restaurant:
 
     def open_restaurant(self):
         print(f"{self.restaurant_name} is now open")
+
+    def set_number_served(self, customers_served):
+        self.number_served = customers_served
+
 
 
 restaurant = Restaurant("Hot n Spicy", "Italian")
@@ -25,6 +31,9 @@ restaurant3 = Restaurant("Sweet","Chinese")
 
 print()
 restaurant2.describe_restaurant()
+restaurant2.set_number_served(5)
+print(f"Total number of customer's sevred today is {restaurant2.number_served}")
 print()
 restaurant3.describe_restaurant()
+print(f"Total number of customer's served for today is {restaurant3.number_served}")
 
