2c4502118210298588fdf115fd6bbd2c2e609985
Chukwukagideon <Chukwukagideon5@gmail.com>
2024-12-18 16:24:32 +0100

    Revert "--creating a module containing multiple classes.--"
    This reverts commit 76aaba2f64d378732cedcfe8c325b82719409247.

--- my_cars.py
+++
@@ -1,8 +0,0 @@
-from car import Car, ElectricCar
-
-
-my_beetle = Car("volkswagen", "beetle", 2020)
-print(my_beetle.get_descriptive_name())
-
-my_tesla = ElectricCar("tesla", "roadster", 2023)
-print(my_tesla.get_descriptive_name())
